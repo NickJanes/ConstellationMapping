d47f98f174aa6ee1cba90d8c707baadc912489e8
Jorel Huerto <jjhue29@gmail.com>
2022-11-19 21:55:03 -0800

    removed debug console log

--- world.js
+++ world.js
@@ -1,6 +1,6 @@
 var margin = {top: 20, right: 80, bottom: 30, left: 50},
-world_width = 900 - margin.left - margin.right,
-world_height = 600 - margin.top - margin.bottom;
+world_width = 750 - margin.left - margin.right,
+world_height = 500 - margin.top - margin.bottom;
 
 var svg_world = d3.select("body")
   .append("svg")
@@ -11,7 +11,7 @@ var svg_world = d3.select("body")
 //world projection (Built with refrence from Mike Bostock) https://bl.ocks.org/mbostock/3682676
 //======================================================================
 var projection = d3.geoWinkel3()
-    .scale(182)
+    .scale(100)
     .translate([world_width / 2, world_height / 2])
     .precision(.1)
 
@@ -39,7 +39,7 @@ svg_world.append("path")
     .attr("d", path);
 
 d3.json("land-50m.json").then(function(world){
-  console.log(topojson.feature(world, world.objects.land))
+  
   svg_world.insert("path", ".graticule")
       .datum(topojson.feature(world, world.objects.land))
       .attr("class", "land")
