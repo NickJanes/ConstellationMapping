99ccbef4041e0829c5a110cd9cc72f9555f8c6dd
Nick Janes <njanes@ucsc.edu>
2022-11-28 14:29:25 -0800

    Updated Constellation for tooltip

--- constellation.js
+++ constellation.js
@@ -3,13 +3,25 @@ var const_margin = {top: 50, right: 50, bottom: 50, left: 50};
 var const_width = 600 - const_margin.left - const_margin.right;
 var const_height = 600 - const_margin.top - const_margin.bottom;
 
-d3.select('body').style('background-color', 'white')
+var star_tooltip = d3.select('body').style('background-color', 'white')
+    .append("div")
+    .attr("class", "tooltip")
+    .style("border-radius", "5px")
+    .style("background-color", "white")
+    .style("padding", "10px")
+    .style("fill", "black")
+    .style("text-align", "center")
+    .style("position", "absolute")
+    .style("pointer-events", "none")
+
 
 var const_svg = d3.select(".container")
     .append('svg')
     .attr('class', 'item')
     .attr("width", const_width + const_margin.left + const_margin.right)
     .attr("height", const_height + const_margin.top + const_margin.bottom)
+    .style("float", "right")
+    .style("margin-right", "100px")
     .style("background-color", "black")
 
 name = 'Ursa Minor'
@@ -17,6 +29,14 @@ abrv = ''
 promises = [d3.csv("con_names.csv"), 
             d3.text("constellation connection.tsv"),
             d3.csv("hygdata_v3.csv")];
+
+const tooly = (event, data) => {
+    star_tooltip
+                .html("" + data.name + "<br>RA:" + data.ra + "<br>Dec" + data.dec)
+                .style('opacity', 1)
+                .style("left", event.pageX + 5 + "px")
+                .style("top", event.pageY + 5 + "px")
+}
 
 var xScale = d3.scaleLinear()
     .range([0, const_width]);
@@ -48,7 +68,7 @@ Promise.all(promises).then(function(files) {
         for(var i = 0; i < stars.length; i++) {
             if(row['id'] == stars[i]) {
                 console.log(row);
-                stars[i] = {x: parseFloat(row['ra']), y: parseFloat(row['dec'])}
+                stars[i] = {x: parseFloat(row['ra']), y: parseFloat(row['dec']), name: row['spect'], ra: row['ra'], dec: row['dec']}
             }
         }
     })
@@ -71,10 +91,15 @@ Promise.all(promises).then(function(files) {
         .data(stars)
         .enter().append("circle")
         .attr("class", "dot")
-        .attr("r", 2)
+        .attr("r", 3)
         .attr("cx", function(d) {return xScale(d.x);})
         .attr("cy", function(d) {return yScale(d.y);})
         .style("fill", "white")
+        .on("mouseover", tooly)
+        .on("mousemove", tooly)
+        .on("mouseleave", ()=>{
+            start_tooltip.attr("opacity", 0)
+        })
     
     for(var i = 0; i < stars.length; i += 2) {
         const_svg.append("line")
@@ -82,7 +107,7 @@ Promise.all(promises).then(function(files) {
         .attr("y1", yScale(stars[i].y))
         .attr("x2", xScale(stars[i + 1].x))
         .attr("y2", yScale(stars[i + 1].y))
-        .attr("stroke", "white")
+        .attr("stroke", "yellow")
         .attr("stroke-width", "2")
         .style("glow", "5px solid yellow");
     }
