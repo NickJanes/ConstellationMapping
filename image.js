622b039ff09aa103a819b98717caf5aa869aacd5
TKJ <josephkteresa@gmail.com>
2022-12-04 14:15:25 -0800

    cleaned up + added serpens caput

--- image.js
+++ image.js
@@ -10,77 +10,6 @@ var const_svg = d3.select(".container")
     .style("float", "right")
     .style("margin-right", "30px");
 
-d3.csv("con_names.csv", function(error, data) {
-    if (error) throw error;
-    console.log(data);
-
-    /*var div = d3.select("body")
-       .append("div")
-       .attr("class", "tooltip")
-       .style("opacity", 0);*/
-
-    var Tooltip = d3.select(".container")
-       .append("div")
-       .style("opacity", 0)
-       .attr("class", "tooltip")
-       .style("background-color", "white")
-       .style("border", "solid")
-       .style("border-width", "2px")
-       .style("border-radius", "5px")
-       .style("padding", "5px");
-   
-
-    var mouseover = function(d) {
-    Tooltip
-      .style("opacity", 1)
-    d3.select(this)
-      .style("stroke", "black")
-      .style("opacity", 1)
-    }
-
-    var mousemove = function(d) {
-    Tooltip
-      .html(d.value)
-      .style("left", (d3.mouse(this)[0]+70) + "px")
-      .style("top", (d3.mouse(this)[1]) + "px")
-    }
-
-    var mouseleave = function(d) {
-    Tooltip
-      .style("opacity", 0)
-    d3.select(this)
-      .style("stroke", "none")
-      .style("opacity", 0.8)
-    }
-
-    /*const_svg.selectAll("rect")
-        .attr("class", "image")
-        .on("mouseover", function(event) {
-            div.transition().duration(200).style("opacity", 1);
-            // tooltip text
-            let toolText = 'Abbreviation: $(data.Abbreviation) <br> Description: $(data.Description) <br> Type: $(data.Type) <br> Source: $(data.Source)'
-            div.html(toolText)
-                .style("left", (event.pageX) + 5 +  "px")
-                .style("top", (event.pageY - 28)  + "px");
-        })
-        // hide tooltip when mouse off
-       .on("mouseout", function(d) {
-            div.transition()
-                .duration(200)
-                .style("opacity", 0)
-    });*/
-
-    const_svg.selectAll()
-    .data(data, function(d) {return "Abbreviation: " + d.Abbreviation +  "Description: " + d.Description +  "<br>: " + d.Type +  "<br>Source: " + d.Source;})
-    .enter()
-    .append("rect")
-    .on("mouseover", mouseover)
-    .on("mousemove", mousemove)
-    .on("mouseleave", mouseleave)
-})
-
-
-
 let updateConstVisualization = (name) => {
     const_svg.selectAll('*').remove();
     
@@ -96,9 +25,19 @@ let updateConstVisualization = (name) => {
     // Sea Sky
     // http://www.seasky.org/constellations/constellations.html
     const_svg.append("svg:image")
-        .attr("xlink:href", "http://www.seasky.org/constellations/assets/images/"
-              +name.toLowerCase().replace(" ", "-").replace("ö", "o")
-              +".jpg")
+        .attr("xlink:href", function(){
+        
+            // Special case, const split into 2 - load both?
+            if (name == "Serpens"){
+                return "http://www.seasky.org/constellations/assets/images/serpens-caput.jpg"
+                //return "http://www.seasky.org/constellations/assets/images/serpens-cauda.jpg"
+            }
+            else{
+                return "http://www.seasky.org/constellations/assets/images/"
+                    +name.toLowerCase().replace(" ", "-").replace("ö", "o")
+                    +".jpg"
+            }
+        })
     
         .on("mouseover", function(d){
         
